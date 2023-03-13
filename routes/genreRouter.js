2e3aabd0d2b8795112858b0c6e93167aea338034
Nalinda Dissanayake <nalindard@gmail.com>
2023-03-13 17:57:23 +0530

    Searching && Filtering methods added

--- routes/genreRouter.js
+++ routes/genreRouter.js
@@ -1,37 +1,123 @@
 import express from 'express'
 import ncs from 'nocopyrightsounds-api'
+import { isValidGenre, isValidNum } from '../helpers/parameterValidator.js'
+
 export const router = express.Router()
 
 // Genres,
 router.get('/:genre/:pageNum', async (req, res) => {
-    try {
-        const songs = await ncs.search(
-            {
-                genre: ncs.Genre.House,
-            },
-            // Page number here
-            2
-        )
-        res.status(200).send(songs)
-    } catch (error) {
-        res.status(500).json({ Error: error })
+    const genre = req.params.genre
+    const pageNum = req.params.pageNum
+    let selectedGenre
+
+    if (isValidGenre(genre) && isValidNum(pageNum)) {
+        switch (genre) {
+            case 'Bass':
+                selectedGenre = ncs.Genre.Bass
+                break
+            case 'Chill':
+                selectedGenre = ncs.Genre.Chill
+                break
+            case 'DrumBass':
+                selectedGenre = ncs.Genre.DrumBass
+                break
+            case 'Drumstep':
+                selectedGenre = ncs.Genre.Drumstep
+                break
+            case 'Dubstep':
+                selectedGenre = ncs.Genre.Dubstep
+                break
+            case 'EDM':
+                selectedGenre = ncs.Genre.EDM
+                break
+            case 'Electronic':
+                selectedGenre = ncs.Genre.Electronic
+                break
+            case 'FutureHouse':
+                selectedGenre = ncs.Genre.FutureHouse
+                break
+            case 'Hardstyle':
+                selectedGenre = ncs.Genre.Hardstyle
+                break
+            case 'House':
+                selectedGenre = ncs.Genre.House
+                break
+            case 'Indie':
+                selectedGenre = ncs.Genre.Indie
+                break
+            case 'MelodicDubstep':
+                selectedGenre = ncs.Genre.MelodicDubstep
+                break
+            case 'Unknown':
+                selectedGenre = ncs.Genre.Unknown
+                break
+            case 'Trap':
+                selectedGenre = ncs.Genre.Trap
+                break
+            case 'GlitchHop':
+                selectedGenre = ncs.Genre.GlitchHop
+                break
+            case 'Phonk':
+                selectedGenre = ncs.Genre.Phonk
+                break
+            case 'FutureBass':
+                selectedGenre = ncs.Genre.FutureBass
+                break
+            case 'BassHouse':
+                selectedGenre = ncs.Genre.BassHouse
+                break
+
+            default:
+                selectedGenre = ncs.Genre.House
+                break
+        }
+
+        try {
+            const songs = await ncs.search(
+                {
+                    // genre: ncs.Genre.House,
+                    genre: selectedGenre,
+                },
+                // Page number here
+                // 2
+                Number(pageNum)
+            )
+            res.status(200).send(songs)
+        } catch (error) {
+            res.status(500).json({ Error: error })
+        }
+    } else {
+        if (!isValidGenre(genre)) {
+            res.status(400).json({
+                Error: `Invalid parameter for genre:${genre}`,
+            })
+        } else if (!isValidNum(pageNum)) {
+            res.status(400).json({
+                Error: `Invalid parameter for page number:${pageNum}`,
+            })
+        } else {
+            res.status(400).json({
+                Error: `Parameters you request is not valid. genre:${genre} && page_number:${pageNum}`,
+            })
+        }
     }
 })
-// Bass = 1,
-//     Chill,
-//     DrumBass,
-//     Drumstep,
-//     Dubstep,
-//     EDM,
-//     Electronic,
-//     FutureHouse,
-//     Hardstyle,
-//     House,
-//     Indie,
-//     MelodicDubstep,
-//     Unknown,
-//     Trap,
-//     GlitchHop,
-//     Phonk,
-//     FutureBass,
-//     BassHouse
+
+// Bass,
+// Chill,
+// DrumBass,
+// Drumstep,
+// Dubstep,
+// EDM,
+// Electronic,
+// FutureHouse,
+// Hardstyle,
+// House,
+// Indie,
+// MelodicDubstep,
+// Unknown,
+// Trap,
+// GlitchHop,
+// Phonk,
+// FutureBass,
+// BassHouse
--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -3,11 +3,14 @@ import ncs from 'nocopyrightsounds-api'
 export const router = express.Router()
 
 // Default pages,
-router.get('/:pageNum', async (req, res) => {
+router.get('/:songName', async (req, res) => {
+    const songName = req.params.songName
     try {
-        const songs = await ncs.getSongs(req.params.pageNum)
+        const songs = await ncs.search({ search: songName })
         res.status(200).send(songs)
     } catch (error) {
         res.status(500).json({ Error: error })
     }
 })
+
+// const songs = await ncs.search({search: ''})
